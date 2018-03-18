e28b2ccaad335f0f04f6bc8cacc017f4a7688f94
JR-SWJTU <752258828@qq.com>
2018-03-18 23:43:55 +0800

    动态切换数据源

--- src/main/java/com/swjtu/SYWeiServers/web/controller/AdminController.java
+++ src/main/java/com/swjtu/SYWeiServers/web/controller/AdminController.java
@@ -1,23 +1,14 @@
 package com.swjtu.SYWeiServers.web.controller;
 
-import com.google.gson.Gson;
-import com.swjtu.SYWeiServers.entity.Admin;
 import com.swjtu.SYWeiServers.entity.StudentExample;
 import com.swjtu.SYWeiServers.mapper.StudentMapper;
-import com.swjtu.SYWeiServers.service.IAdminService;
 import com.swjtu.SYWeiServers.util.DataSourceFactory;
 import com.swjtu.SYWeiServers.util.JsonResult;
 import com.swjtu.SYWeiServers.util.enums.StatusCode;
-import net.sf.json.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.annotation.Resource;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Administrator on 2017/10/18.
@@ -26,67 +17,6 @@ import java.util.Map;
 @RequestMapping("admin")
 @ResponseBody
 public class AdminController {
-
-    @Resource
-    private IAdminService adminService;
-
-    @RequestMapping(value="/register", method = RequestMethod.POST)
-    public JsonResult register(@RequestBody Admin admin) throws  Exception{
-        System.out.println("注册信息：" + new Gson().toJson(admin));
-        adminService.register(admin);
-        return JsonResult.build(StatusCode.SUCCESS);
-    }
-
-    @RequestMapping(value="/login", method = RequestMethod.POST)
-    public JsonResult login(@RequestBody Admin admin) throws  Exception{
-        Admin admin1 = adminService.login(admin);
-        JSONObject jsonObject = JSONObject.fromObject(admin1);
-        jsonObject.remove("password");
-        return JsonResult.build(StatusCode.SUCCESS,jsonObject);
-    }
-
-    /**
-     * 添加管理员信息
-     * @param admin
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping(value="", method = RequestMethod.POST)
-    public JsonResult addAdmin(@RequestBody Admin admin) throws Exception{
-        adminService.addAdmin(admin);
-        return JsonResult.build(StatusCode.SUCCESS);
-    }
-
-    /**批量删除管理员*/
-    @RequestMapping(value = "", method = RequestMethod.DELETE)
-    public JsonResult deleteDepartment(@RequestBody Map map) {
-        List ids =  (List)map.get("ids");
-        adminService.deleteAdmin(ids);
-        return JsonResult.ok("");
-    }
-
-    /**
-     * 分页查询管理员信息
-     * @param pageNum
-     * @param pageSize
-     * @return
-     */
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public JsonResult queryAdmins(Integer pageNum, Integer pageSize){
-//        PageResult admins = adminService.getAdminForPage(pageNum, pageSize);
-        return JsonResult.build(StatusCode.SUCCESS, adminService.test(1,10));
-    }
-
-    /**
-     * 更新管理员信息
-     * @param admin
-     * @return
-     */
-    @RequestMapping(value = "", method = RequestMethod.PATCH)
-    public JsonResult updateAdmin(@RequestBody Admin admin) throws Exception{
-        adminService.updateAdmin(admin);
-        return JsonResult.build(StatusCode.SUCCESS);
-    }
 
     @RequestMapping(value = "/testlogin",method = RequestMethod.GET)
     public JsonResult testLogin( Integer id) throws Exception{
